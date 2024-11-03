7331b12fe637c859ba53e0c834d89b45e5e80d38
Reezan Visram <reezan.visram@rogers.com>
2024-11-03 16:42:36 -0500

    add counter

--- autotest.sh
+++ autotest.sh
@@ -4,10 +4,17 @@ echo "To add more benchmarks, copy the .x files for the desired benchmarks from 
 
 source $1/env.sh
 
+num_benchmarks=0
+num_passed=0
 for xfile in $1/verif/data/*; do
+    num_benchmarks += 1
     benchmark=$(basename "$xfile" .x)
 
     make -C $1/verif/scripts -s run TEST=test_pd MEM_PATH=$1/verif/data/$benchmark.x
 
-    cargo run --bin betterpd4diff $1/verif/golden/$benchmark.trace $1/verif/sim/verilator/test_pd/$benchmark.trace
+    if ! cargo run --bin betterpd4diff $1/verif/golden/$benchmark.trace $1/verif/sim/verilator/test_pd/$benchmark.trace | grep -q "At least one error"; then
+        num_passed += 1
+    fi
+    
+    echo "$num_passed/$num_benchmarks passed! See output for details on (potential) error messages"
 done
